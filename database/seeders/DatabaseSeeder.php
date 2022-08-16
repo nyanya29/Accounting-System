1e55f91d3ec9797cc659542719c93b815f0b784f
chambokk <chamae12anecito@gmail.com>
2022-08-16 11:41:34 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
             FirstUserSeed::class,
             PermissionUserSeeder::class,
             PermissionsSeeder::class,
-            Municipalities::class,
-            Barangays::class,
+            // Municipalities::class,
+            // Barangays::class,
         ]);
 
         \App\Models\User::factory(100)->create();
