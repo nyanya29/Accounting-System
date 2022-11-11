1c903b8166154efe180cbbc5173a01bad2c35310
chambokk <chamae12anecito@gmail.com>
2022-11-11 13:27:56 +0800

    update of jevd crud

--- public/js/resources_js_Pages_Jevh_JevReport_vue.js
+++ public/js/resources_js_Pages_Jevh_JevReport_vue.js
@@ -11,11 +11,56 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _Shared_BackButton_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../Shared/BackButton.vue */ "./resources/js/Shared/BackButton.vue");
+/* harmony import */ var _inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @inertiajs/inertia-vue3 */ "./node_modules/@inertiajs/inertia-vue3/dist/index.js");
+/* harmony import */ var _Shared_BackButton_vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../Shared/BackButton.vue */ "./resources/js/Shared/BackButton.vue");
+
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   components: {
-    BackButton: _Shared_BackButton_vue__WEBPACK_IMPORTED_MODULE_0__["default"]
+    BackButton: _Shared_BackButton_vue__WEBPACK_IMPORTED_MODULE_1__["default"]
+  },
+  data: function data() {
+    return {
+      myReportsModal: null,
+      funds: {},
+      jevtype: [{
+        value: 1,
+        name: "Collection"
+      }, {
+        value: 2,
+        name: "Check Disbursement"
+      }, {
+        value: 3,
+        name: "Cash Disbursement"
+      }, {
+        value: 4,
+        name: "General"
+      }, {
+        value: 5,
+        name: "ADA"
+      }, {
+        value: 6,
+        name: "Procurement"
+      }],
+      form: (0,_inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_0__.useForm)({
+        FUND_SCODE: "",
+        FJEVTYP: "",
+        date_from: "",
+        date_to: ""
+      })
+    };
+  },
+  mounted: function mounted() {
+    this.getAllFunds();
+  },
+  methods: {
+    getAllFunds: function getAllFunds() {
+      var _this = this;
+
+      axios.post('get-fund-details').then(function (response) {
+        _this.funds = response.data;
+      });
+    }
   }
 });
 
@@ -53,10 +98,77 @@ var _hoisted_5 = {
 var _hoisted_6 = {
   "class": "card"
 };
-
-var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"card-body\"><div class=\"col-md-12\"><div class=\"row g-2\"><div class=\"col-md\"><label for=\"\">From</label><input type=\"date\" class=\"form-control mt-1 pt-1 mb-1\"></div><div class=\"col-md\"><label for=\"\">To</label><input type=\"date\" class=\"form-control mt-1 pt-1 mb-1\"></div></div><div class=\"row g-2\"><div class=\"col-md\"><label for=\"colFormLabelSm\" class=\"col-sm-2 col-form-label\">Fund</label><select class=\"form-select mt-1 pt-1 mb-1\"><!-- &lt;option v-for=&quot;item in funds&quot; :value=&quot;item.FUND_SCODE&quot; :key=&quot;item.recid&quot;&gt; {{ item.FUNDDETAIL_NAME}}&lt;/option&gt; --></select></div></div><div class=\"col-md\"><label for=\"colFormLabelSm\" class=\"col-sm-2 col-form-label\">Jev Type</label><select class=\"form-select mt-1 pt-1 mb-1\"><!-- &lt;option v-for=&quot;(item,index) in jevtype&quot; :value=&quot;item.value&quot; :key=&quot;index&quot;&gt; {{ item.name}}&lt;/option&gt; --></select></div></div></div>", 1);
-
+var _hoisted_7 = {
+  "class": "card-body"
+};
 var _hoisted_8 = {
+  "class": "col-md-12"
+};
+var _hoisted_9 = {
+  "class": "row g-2"
+};
+var _hoisted_10 = {
+  "class": "col-md"
+};
+
+var _hoisted_11 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "for": ""
+}, "From", -1
+/* HOISTED */
+);
+
+var _hoisted_12 = {
+  "class": "col-md"
+};
+
+var _hoisted_13 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "for": ""
+}, "To", -1
+/* HOISTED */
+);
+
+var _hoisted_14 = {
+  "class": "row g-2"
+};
+var _hoisted_15 = {
+  "class": "col-md"
+};
+
+var _hoisted_16 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "for": "colFormLabelSm",
+  "class": "col-sm-2 col-form-label"
+}, "Fund", -1
+/* HOISTED */
+);
+
+var _hoisted_17 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("option", {
+  disabled: "",
+  value: ""
+}, " Select Fund", -1
+/* HOISTED */
+);
+
+var _hoisted_18 = ["value"];
+var _hoisted_19 = {
+  "class": "col-md"
+};
+
+var _hoisted_20 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "for": "colFormLabelSm",
+  "class": "col-sm-2 col-form-label"
+}, "Jev Type", -1
+/* HOISTED */
+);
+
+var _hoisted_21 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("option", {
+  disabled: "",
+  value: ""
+}, " Select Jev type", -1
+/* HOISTED */
+);
+
+var _hoisted_22 = ["value"];
+var _hoisted_23 = {
   "class": "modal-footer p-1"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
@@ -64,11 +176,59 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_back_button, {
     href: '/jevh/index'
-  })]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [_hoisted_7, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
+  })]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_9, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_10, [_hoisted_11, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+    type: "date",
+    "class": "form-control mt-1 pt-1 mb-1",
+    "onUpdate:modelValue": _cache[0] || (_cache[0] = function ($event) {
+      return $data.form.date_from = $event;
+    })
+  }, null, 512
+  /* NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $data.form.date_from]])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [_hoisted_13, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+    type: "date",
+    "class": "form-control mt-1 pt-1 mb-1",
+    "onUpdate:modelValue": _cache[1] || (_cache[1] = function ($event) {
+      return $data.form.date_to = $event;
+    })
+  }, null, 512
+  /* NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $data.form.date_to]])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_14, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_15, [_hoisted_16, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("select", {
+    "class": "form-select mt-1 pt-1 mb-1",
+    "onUpdate:modelValue": _cache[2] || (_cache[2] = function ($event) {
+      return $data.form.FUND_SCODE = $event;
+    })
+  }, [_hoisted_17, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.funds, function (item) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("option", {
+      value: item.FUND_SCODE,
+      key: item.recid
+    }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.FUNDDETAIL_NAME), 9
+    /* TEXT, PROPS */
+    , _hoisted_18);
+  }), 128
+  /* KEYED_FRAGMENT */
+  ))], 512
+  /* NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelSelect, $data.form.FUND_SCODE]])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_19, [_hoisted_20, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("select", {
+    "class": "form-select mt-1 pt-1 mb-1",
+    "onUpdate:modelValue": _cache[3] || (_cache[3] = function ($event) {
+      return $data.form.FJEVTYP = $event;
+    })
+  }, [_hoisted_21, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.jevtype, function (item, index) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("option", {
+      value: item.value,
+      key: index
+    }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.name), 9
+    /* TEXT, PROPS */
+    , _hoisted_22);
+  }), 128
+  /* KEYED_FRAGMENT */
+  ))], 512
+  /* NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelSelect, $data.form.FJEVTYP]])])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_23, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
     type: "button",
     "class": "btn btn-primary mt-3",
-    onClick: _cache[0] || (_cache[0] = function ($event) {
-      return _ctx.submit();
+    onClick: _cache[4] || (_cache[4] = function ($event) {
+      return _ctx.print();
     })
   }, "Print")])])])])]);
 }
--- resources/js/Pages/Jevh/JevReport.vue
+++ resources/js/Pages/Jevh/JevReport.vue
@@ -13,31 +13,33 @@
                             <div class="row g-2">
                                 <div class="col-md">
                                     <label for="">From</label>
-                                    <input type="date" class="form-control mt-1 pt-1 mb-1">
+                                    <input type="date" class="form-control mt-1 pt-1 mb-1" v-model="form.date_from">
                                 </div>
                                 <div class="col-md">
                                     <label for="">To</label>
-                                    <input type="date" class="form-control mt-1 pt-1 mb-1">
+                                    <input type="date" class="form-control mt-1 pt-1 mb-1" v-model="form.date_to">
                                 </div>
                             </div>
                             <div class="row g-2">
                                 <div class="col-md">
                                     <label for="colFormLabelSm" class="col-sm-2 col-form-label">Fund</label>
-                                    <select class="form-select mt-1 pt-1 mb-1">
-                                        <!-- <option v-for="item in funds" :value="item.FUND_SCODE" :key="item.recid"> {{ item.FUNDDETAIL_NAME}}</option> -->
+                                    <select class="form-select mt-1 pt-1 mb-1" v-model="form.FUND_SCODE">
+                                        <option disabled value=""> Select Fund</option>
+                                        <option v-for="item in funds" :value="item.FUND_SCODE" :key="item.recid"> {{ item.FUNDDETAIL_NAME}}</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-md">
                                 <label for="colFormLabelSm" class="col-sm-2 col-form-label">Jev Type</label>
-                                <select class="form-select mt-1 pt-1 mb-1">
-                                    <!-- <option v-for="(item,index) in jevtype" :value="item.value" :key="index"> {{ item.name}}</option> -->
-                                </select>
+                                    <select class="form-select mt-1 pt-1 mb-1" v-model="form.FJEVTYP">
+                                        <option disabled value=""> Select Jev type</option>
+                                        <option v-for="(item,index) in jevtype" :value="item.value" :key="index"> {{ item.name}}</option>
+                                    </select>
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer p-1">
-                        <button type="button" class="btn btn-primary mt-3" @click="submit()">Print</button>
+                        <button type="button" class="btn btn-primary mt-3" @click="print()">Print</button>
                     </div>
                 </div>
             </div>
@@ -46,14 +48,44 @@
 </template>
 
 <script>
+import { useForm } from "@inertiajs/inertia-vue3"
 import BackButton from "../../Shared/BackButton.vue"
 
 export default({
     components: {
         BackButton
-    }
+    },
+    data() {    
+        return {
+            myReportsModal: null,
+            funds: {},
+            jevtype:[
+                {value:1, name:"Collection"},
+                {value:2, name:"Check Disbursement"},
+                {value:3, name:"Cash Disbursement"},
+                {value:4, name:"General"},
+                {value:5, name:"ADA"},
+                {value:6, name:"Procurement"},
+            ],
+            form: useForm({
+                FUND_SCODE: "",
+                FJEVTYP:"",
+                date_from:"",
+                date_to:"",
+            }),
+        }
+    },
+    
+    mounted() {
+        this.getAllFunds()
+    },
+    methods: {
+        getAllFunds() {
+            axios.post('get-fund-details').then( response => {
+                this.funds = response.data
+            })
+        },
+    },
 })
 
 </script>
-
-  components: { BackButton },
