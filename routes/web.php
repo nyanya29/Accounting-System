90311ee4f9ddd0cd60f466585a14d379efae486e
nyanya29 <ralphjadegarcia29@gmail.com>
2023-01-15 15:14:10 +0800

    done details report

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,6 @@ Route::middleware('auth')->group(function() {
 Route::get('print-jevd', [JevdReportsController::class, 'jevdReport']);
 Route::get('print-jevtype', [JevdReportsController::class, 'jevdTypeReport']);
 Route::get('jevtype-second-report', [JevdReportsController::class, 'jevdTypeSecondReport']);
-Route::prefix('print')->group(function() {
-    Route::get('checkdisbjournal', [JevdReportsController::class, 'checkDisbursementJournal']);
-});
+///
+Route::get('check-disb-journal', [JevdReportsController::class, 'checkDisbursementJournal']);
+// Route::get('check-disb-journal', [JevdReportsController::class, 'jevdTypeReport']);
